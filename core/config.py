59d06db7e25a73686773695e7b57243182213790
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2026-03-25 09:51:25 +0530

    supabase config

--- core/config.py
+++ core/config.py
@@ -2,6 +2,7 @@ import os
 import platform
 import pytesseract
 from dotenv import load_dotenv
+from supabase import create_client, Client
  
 load_dotenv()
  
@@ -11,3 +12,8 @@ if platform.system() == "Windows":
  
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 MODEL              = "arcee-ai/trinity-large-preview:free"
+
+# Supabase
+SUPABASE_URL: str    = os.getenv("SUPABASE_URL", "")
+SUPABASE_ANON_KEY: str    = os.getenv("SUPABASE_ANON_KEY", "")
+supabase: Client     = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
